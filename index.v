c9cf46de3e52983b8188cb5cdca9ef516bd430a9
yoshihiro503 <y.imai@ocaml.jp>
2012-05-25 11:04:53 +0900

    いろいろお試し。

--- index.v
+++ index.v
@@ -1,4 +1,26 @@
 (**
- * Hello coqdoc
- * This is the yoshihiro503's homepage.
+   This is the yoshihiro503's homepage.
  *)
+
+(**
+   #<pre>lowhtml</pre>#
+
+   * Section
+   ** SubSection
+   *** SubSub?
+   **** SubSubSub?
+
+   - list item a
+   - list item b
+   - list item c
+
+   ----
+
+   <<
+   v e r b a t i m  string
+   >>
+ *)
+
+(* begin hide *)
+Let x := 3. (*隠したいコード*)
+(* end hide *)
