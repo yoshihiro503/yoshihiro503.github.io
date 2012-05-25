3375c9cdee46e993eed378551dee39326e1c2a39
yoshihiro503 <y.imai@ocaml.jp>
2012-05-25 12:54:52 +0900

    プロフィール

--- index.v
+++ index.v
@@ -1,25 +1,12 @@
 (**
-   This is the yoshihiro503's homepage.
+   * Yoshihiro Imai
+
+   - Twitter: #<a href="http://twitter.com/yoshihiro503">@yoshihiro503</a>#
+   - Google+: #<a href="http://gplus.to/yoshihiro503">yoshihiro503</a>#
+   - Blog: #<a href="http://d.hatena.ne.jp/yoshihiro503">id:yoshihiro503</a>#
+   - facebook: #<a href="http://facebook.com/yosh.imai">Yoshihiro Imai</a>#
  *)
 
-(**
-   #<pre>lowhtml</pre>#
-
-   * Section
-   ** SubSection
-   *** SubSub?
-   **** SubSubSub?
-
-   - list item a
-   - list item b
-   - list item c
-
-   ----
-
-   <<
-   v e r b a t i m  string
-   >>
- *)
 
 (* begin hide *)
 Let x := 3. (*隠したいコード*)
