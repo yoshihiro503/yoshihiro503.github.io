b4b9f48bb26b0c101e640d9b337c0b6726f8e02d
yoshihiro503 <y.imai@ocaml.jp>
2012-06-01 13:14:21 +0900

    項目をいろいろ増やした。

--- src/home_en.v
+++ src/home_en.v
@@ -5,6 +5,41 @@
    - Google+: #<a href="http://gplus.to/yoshihiro503">yoshihiro503</a>#
    - Blog: #<a href="http://d.hatena.ne.jp/yoshihiro503">id:yoshihiro503</a>#
    - facebook: #<a href="http://facebook.com/yosh.imai">Yoshihiro Imai</a>#
+
+   * Software
+   - #<a href="http://proofcafe.org/wiki/ocamltter">ocamltter</a>#
+   - #<a href="http://proofcafe.org/wiki/Coq2Scala">coq2scala</a>#
+
+   * Books
+
+   #<iframe src="http://rcm-jp.amazon.co.jp/e/cm?t=proofcafe-22&o=9&p=8&l=as1&asins=4839923116&ref=tf_til&fc1=000000&IS2=1&lt1=_blank&m=amazon&lc1=0000FF&bc1=000000&bg1=FFFFFF&f=ifr" style="width:120px;height:240px;" scrolling="no" marginwidth="0" marginheight="0" frameborder="0"></iframe>#
+
+   * Talks & Lectures
+   - 2007 Leightweight Language Spirit
+   - 2008 Leightweight Language Future
+   - 2009 #<a href="">Open Source Conference</a>#
+   - 2010 #<a href="">Coq un</a>#
+   - 2011 #<a href="">QCon Tokyo 2011</a>#
+   - 2011 #<a href="http://topse.or.jp/docs/?q=node/26">Coq tutorial - Top SE</a>#
+   - 2011 Advanced Study @Kyoto-univ
+   - 2011 #<a href="http://www.cshe.nagoya-u.ac.jp/service/grad/math_120127.html">Career up Seminar</a>#
+   - 2012 #<a href="http://topse.or.jp/docs/?q=node/36">Coq special seminar - Top SE</a>#
+   - 2012 #<a href="http://proofcafe.org/~yoshihiro503/mlnagoya.pdf">ML Nagoya</a>#
+
+   * Academic Activity
+   - #<a href="http://www.info.kochi-tech.ac.jp/ppl2011/">
+   Programming and Programming Languages 2011
+   </a># (program committee)
+   - #<a href="http://www.ipsj.or.jp/09sig/kaikoku/2010/PRO83.html">PRO83</a>#
+
+   * Study Circle Membership
+   - #<a href="http://proofcafe.org/wiki/">ProofCafe</a>#
+   - #<a href="http://www.itpl.co.jp/ocaml-nagoya/">ocaml-nagoya</a>#
+   - #<a href="http://nagoyascala.yoshihiro503.cloudbees.net/">Nagoya Scala</a>#
+   - #<a href="http://proofcafe.org/doehas/">Doerya Haskell</a>#
+   - #<a href="http://proofcafe.org/instiki/kctnagoya/show/HomePage">KCT Nagoya</a>#
+   - #<a href="">Nagoya SML##</a>#
+
  *)
 
 
