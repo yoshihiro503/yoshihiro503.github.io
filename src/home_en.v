c81fb463bf78ae5736bfdf526d9ae982a826a2d7
Yoshihiro503 <yoshihiro503@proofcafe.org>
2013-04-09 16:59:31 +0900

    add TAPL to my Books

--- src/home_en.v
+++ src/home_en.v
@@ -30,7 +30,9 @@
 
    * Books
 
-   #<iframe src="http://rcm-jp.amazon.co.jp/e/cm?t=proofcafe-22&o=9&p=8&l=as1&asins=4839923116&ref=tf_til&fc1=000000&IS2=1&lt1=_blank&m=amazon&lc1=0000FF&bc1=000000&bg1=FFFFFF&f=ifr" style="width:120px;height:240px;" scrolling="no" marginwidth="0" marginheight="0" frameborder="0"></iframe>#
+   #<iframe src="http://rcm-jp.amazon.co.jp/e/cm?t=proofcafe-22&o=9&p=8&l=as1&asins=4839923116&ref=tf_til&fc1=000000&IS2=1&lt1=_blank&m=amazon&lc1=0000FF&bc1=000000&bg1=FFFFFF&f=ifr" style="width:120px;height:240px;" scrolling="no" marginwidth="0" marginheight="0" frameborder="0"></iframe>
+
+   <iframe src="http://rcm-jp.amazon.co.jp/e/cm?t=proofcafe-22&o=9&p=8&l=as1&asins=4274069117&ref=qf_sp_asin_til&fc1=000000&IS2=1&lt1=_blank&m=amazon&lc1=0000FF&bc1=000000&bg1=FFFFFF&f=ifr" style="width:120px;height:240px;" scrolling="no" marginwidth="0" marginheight="0" frameborder="0"></iframe>#
 
    * Talks & Lectures
    - 2007 Leightweight Language Spirit
--- src/home_ja.v
+++ src/home_ja.v
@@ -28,9 +28,11 @@
    - #<a href="http://proofcafe.org/wiki/ocamltter">ocamltter</a>#
    - #<a href="http://proofcafe.org/wiki/Coq2Scala">coq2scala</a>#
 
-   * 本
+   * 著書
 
-   #<iframe src="http://rcm-jp.amazon.co.jp/e/cm?t=proofcafe-22&o=9&p=8&l=as1&asins=4839923116&ref=tf_til&fc1=000000&IS2=1&lt1=_blank&m=amazon&lc1=0000FF&bc1=000000&bg1=FFFFFF&f=ifr" style="width:120px;height:240px;" scrolling="no" marginwidth="0" marginheight="0" frameborder="0"></iframe>#
+   #<iframe src="http://rcm-jp.amazon.co.jp/e/cm?t=proofcafe-22&o=9&p=8&l=as1&asins=4839923116&ref=tf_til&fc1=000000&IS2=1&lt1=_blank&m=amazon&lc1=0000FF&bc1=000000&bg1=FFFFFF&f=ifr" style="width:120px;height:240px;" scrolling="no" marginwidth="0" marginheight="0" frameborder="0"></iframe>
+
+   <iframe src="http://rcm-jp.amazon.co.jp/e/cm?t=proofcafe-22&o=9&p=8&l=as1&asins=4274069117&ref=qf_sp_asin_til&fc1=000000&IS2=1&lt1=_blank&m=amazon&lc1=0000FF&bc1=000000&bg1=FFFFFF&f=ifr" style="width:120px;height:240px;" scrolling="no" marginwidth="0" marginheight="0" frameborder="0"></iframe>#
 
    * 発表、講演
    - 2007 Leightweight Language 魂
