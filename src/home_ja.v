d80eeab84d16d2c07f92b4fa54993735b5d8575d
Yoshihiro Imai <yoshihiro503@proofcafe.org>
2022-06-13 01:25:49 +0900

    introduce link of books

--- src/home_ja.v
+++ src/home_ja.v
@@ -37,6 +37,8 @@
    - #<a href="http://proofcafe.org/wiki/Coq2Scala">coq2scala</a>#
 
    ** 著書
+   - #<a href="https://www.amazon.co.jp/dp/4274069117">型システム入門 −プログラミング言語と型の理論</a>#
+   - #<a href="https://www.amazon.co.jp/dp/4839923116/">入門OCaml ~プログラミング基礎と実践理解</a>#
 
    #<iframe src="https://rcm-jp.amazon.co.jp/e/cm?t=proofcafe-22&o=9&p=8&l=as1&asins=4839923116&ref=tf_til&fc1=000000&IS2=1&lt1=_blank&m=amazon&lc1=0000FF&bc1=000000&bg1=FFFFFF&f=ifr" style="width:120px;height:240px;" scrolling="no" marginwidth="0" marginheight="0" frameborder="0"></iframe>
 
